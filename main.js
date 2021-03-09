97c4d13d20861c822c27a18c23b6e8f752d353da
Ronny <ronnykuruvilla@gmail.com>
2021-03-09 16:01:48 -0500

    almost done

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(ad){
+    if (ad.userRole === 'ADMIN'){
+        return true;
+    }else{
+        return false;
+    }
+}
 
+function getEmail(email){
+    return ((email.firstName.toLowerCase()) + '.' + (email.lastName.toLowerCase()) + '@codeimmersives.com');
 
+}
+
+function getPlaylistLength(song){
+    return song.songs.length;
+}
+
+function getHardestHomework(score){
+    if (score.length === 0){
+        return "";
+    }
+
+}
+
+function createPhonebook(name,phone){
+    let obj = {};
+    for (let i = 0; i < name.length; i++){
+        obj[name[i]] = phone[i];
+    }
+    return obj;
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
